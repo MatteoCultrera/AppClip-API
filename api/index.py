d4948ec5bcf4ba804e93a390abbb3f879a227f0a
Matteo Cultrera <matteo.cultrera@qonto.com>
2023-03-17 10:50:50 +0100

    Test

--- api/index.py
+++ api/index.py
@@ -6,5 +6,5 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
+        self.wfile.write('2F1add9&p1=a95bc7b7&p2=0b2752a2f3faaa85835cfdfc5f3'.encode('utf-8'))
         return
