c441c81e403ee9b4ad1a59e87b4cd6e9aef7c0c0
Matteo Cultrera <mcultrera94@icloud.com>
2023-03-20 11:24:12 +0100

    update index file

--- api/index.py
+++ api/index.py
@@ -1,10 +1,12 @@
 from http.server import BaseHTTPRequestHandler
 
+token='test'
+
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        self.wfile.write('fd203785f167eb9c3c3f1be179d1e6075664b514'.encode('utf-8'))
+        self.wfile.write(token.encode('utf-8'))
         return
